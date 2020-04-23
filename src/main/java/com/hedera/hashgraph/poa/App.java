b5294826be210a7d1012e4911cc661b589c042d4
Ryan Leckey <ryan@launchbadge.com>
2020-04-23 02:35:21 -0700

    feat: very basic prove api using in-memory hashset

--- src/main/java/com/hedera/hashgraph/poa/App.java
+++ src/main/java/com/hedera/hashgraph/poa/App.java
@@ -3,16 +3,25 @@ package com.hedera.hashgraph.poa;
 import com.google.common.flogger.FluentLogger;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static io.vertx.core.Vertx.vertx;
+import static io.vertx.ext.web.Router.router;
 
 public class App extends AbstractVerticle {
     private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
+    private Set<String> actions = new HashSet<>();
+
     private HttpServer httpServer;
 
     public static void main(String[] args) {
-        Vertx.vertx().deployVerticle(new App());
+        vertx().deployVerticle(new App());
     }
 
     @Override
@@ -20,11 +29,16 @@ public class App extends AbstractVerticle {
         // saves the server so we can cleanly exit later
         httpServer = vertx.createHttpServer();
 
-        httpServer.requestHandler(req -> {
-            // terminate immediately with "Hello, World"
-            req.response().end("Hello, World");
-        });
+        var router = router(vertx);
+        router.errorHandler(500, this::handleError);
 
+        // handle accepting request bodies but cap it at 4 Ki
+        router.route().handler(BodyHandler.create().setBodyLimit(4096));
+
+        router.post("/v1/action").handler(this::handleSubmitAction);
+        router.get("/v1/action").handler(this::handleFindAction);
+
+        httpServer.requestHandler(router);
         httpServer.listen(8080, "0.0.0.0", v -> {
             if (v.succeeded()) {
                 logger.atInfo().log("listening on http://0.0.0.0:8080/");
@@ -39,5 +53,51 @@ public class App extends AbstractVerticle {
     @Override
     public void stop(Promise<Void> stopPromise) {
         httpServer.close(stopPromise);
+    }
+
+    private void handleError(RoutingContext rx) {
+        logger.atSevere().withCause(rx.failure()).log();
+        rx.response().setStatusCode(500).end();
+    }
+
+    private void handleSubmitAction(RoutingContext rx) {
+        var payload = rx.getBodyAsString();
+        var res = rx.response();
+
+        if (payload.isEmpty()) {
+            // the payload should at least be *something*
+            res.setStatusCode(400).end();
+            return;
+        }
+
+        // TODO: submit to HCS
+
+        // TODO: use persistence
+        // prove
+
+        logger.atInfo().log(payload);
+
+        actions.add(payload);
+
+        res.setStatusCode(201).end();
+    }
+
+    private void handleFindAction(RoutingContext rx) {
+        var res = rx.response();
+
+        // TODO: use persistence
+
+        // TODO: lookup by ID
+
+        var queryPayload = rx.request().getParam("payload");
+        if (queryPayload != null) {
+            if (actions.contains(queryPayload)) {
+                res.end("found");
+                return;
+            }
+        }
+
+        // no dice
+        res.setStatusCode(404).end();
     }
 }
